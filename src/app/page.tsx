5675ed87c6e8719d5cd18203666017fe753f0543
CelsoPereiraDev <celsopereira.dev@gmail.com>
2024-03-21 14:45:26 -0300

    feat: add tailwind and change mock to first test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,33 @@
 'use client'
-
+import '@/app/globals.css'
 import { useEffect, useState } from "react";
 import { distributePlayers, Player, Team } from "@/utils/createTeam";
 
 
 const data: Player[] = [
-  { name: "Lucas Silva", overall: 78, position: "Forward" },
-  { name: "Gabriel Santos", overall: 68, position: "Midfielder" },
-  { name: "Rafael Oliveira", overall: 82, position: "Defender" },
-  { name: "Carlos Pereira", overall: 55, position: "Goalkeeper" },
-  { name: "Matheus Costa", overall: 72, position: "Forward" },
-  { name: "Felipe Almeida", overall: 63, position: "Midfielder" },
-  { name: "Fernando Barbosa", overall: 79, position: "Defender" },
-  { name: "Ricardo Nunes", overall: 58, position: "Goalkeeper" },
-  { name: "Gustavo Silva", overall: 75, position: "Forward" },
-  { name: "Pedro Mendes", overall: 65, position: "Midfielder" },
-  { name: "André Santos", overall: 81, position: "Defender" },
-  { name: "João Oliveira", overall: 52, position: "Goalkeeper" },
-  { name: "Marcos Lima", overall: 70, position: "Forward" },
-  { name: "Renato Fernandes", overall: 60, position: "Midfielder" },
-  { name: "Bruno Oliveira", overall: 80, position: "Defender" },
-  { name: "Rodrigo Costa", overall: 50, position: "Goalkeeper" },
-  { name: "Daniel Alves", overall: 77, position: "Forward" },
-  { name: "Jorge Santos", overall: 66, position: "Midfielder" },
-  { name: "Leonardo Oliveira", overall: 83, position: "Defender" },
-  { name: "Antônio Silva", overall: 56, position: "Goalkeeper" },
-  { name: "Vinícius Mendonça", overall: 73, position: "Forward" }
+  { "name": "2008", "overall": 28, "position": "Midfielder" },
+  { "name": "Adriano", "overall": 49, "position": "Midfielder" },
+  { "name": "Bolsonaro", "overall": 53, "position": "Forward" },
+  { "name": "Celso", "overall": 77, "position": "Defender" },
+  { "name": "Danniboy", "overall": 79, "position": "Goalkeeper" },
+  { "name": "Daro", "overall": 78, "position": "Midfielder" },
+  { "name": "Du", "overall": 53, "position": "Forward" },
+  { "name": "Eric", "overall": 42, "position": "Midfielder" },
+  { "name": "Fer", "overall": 59, "position": "Midfielder" },
+  { "name": "Gabriel", "overall": 59, "position": "Defender" },
+  { "name": "Gabrielzinho", "overall": 54, "position": "Forward" },
+  { "name": "Guilherme", "overall": 27, "position": "Forward" },
+  { "name": "Gustavo", "overall": 55, "position": "Midfielder" },
+  { "name": "Igor", "overall": 55, "position": "Midfielder" },
+  { "name": "João", "overall": 87, "position": "Defender" },
+  { "name": "João da Radio", "overall": 37, "position": "Forward" },
+  { "name": "João Primo", "overall": 65, "position": "Midfielder" },
+  { "name": "Leo", "overall": 41, "position": "Defender" },
+  { "name": "Matheus", "overall": 63, "position": "Goalkeeper" },
+  { "name": "Preto", "overall": 90, "position": "Forward" },
+  { "name": "Tcharlly", "overall": 58, "position": "Midfielder" }
 ];
+
 
 export default function Home() {
   const [teams, setTeams] = useState<Team[]>([]);
@@ -39,12 +40,12 @@ export default function Home() {
   return (
     <>
       {teams.map((team, index) => (
-        <div key={index}>
-          <p>Time {index + 1} Overall:{team.overall}</p>
+        <div key={index} className="p-4  my-4 bg-white text-black w-fit rounded-lg min-w-[360px]">
+          <p className='mb-4 text-center'>Time {index + 1} Overall:{team.overall}</p>
           <ul>
             {team.players.map((player, playerIndex) => ( 
               <li key={playerIndex}>
-                {player.name} - Overall: {player.overall}
+                {player.name}
               </li>
             ))}
           </ul>
