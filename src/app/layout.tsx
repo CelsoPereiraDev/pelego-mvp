25fa115b1a17aab9750d42b617f9459f84b0b716
CelsoPereiraDev <celsopereira.dev@gmail.com>
2024-03-22 09:09:10 -0300

    feat: add cruyff sans font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,16 @@
+
+import localFont from 'next/font/local'
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const otherFontFamily = localFont({
+  src: '../../public/sans.woff',
+  display: 'swap',
+  variable: '--font-main',
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +23,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={` ${otherFontFamily.variable}`}>
+      <body >{children}</body>
     </html>
   );
 }
