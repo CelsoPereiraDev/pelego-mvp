4573f5de818bb872e2d3a2b6ea46c36f6eff75af
CelsoPereiraDev <celsopereira.dev@gmail.com>
2024-03-28 14:11:04 -0300

    feat: edit path to mock

---
+++ src/utils/mockPlayers.ts
@@ -0,0 +1,345 @@
+import { NH } from "./images"
+
+export default function PlayerMock() {
+    return(
+        [
+            { 
+                "name": "2008", 
+                "overall": { 
+                    "pace": "", 
+                    "shooting": "", 
+                    "passing": "", 
+                    "dribble": "", 
+                    "defense": "", 
+                    "physics": "", 
+                    "overall": 28 
+                }, 
+                "country": "", 
+                "team": "", 
+                "image": "", 
+                "position": "MEI" 
+            },
+            { 
+                "name": "Adriano", 
+                "overall": { 
+                    "pace": "", 
+                    "shooting": "", 
+                    "passing": "", 
+                    "dribble": "", 
+                    "defense": "", 
+                    "physics": "", 
+                    "overall": 61 
+                }, 
+                "country": "", 
+                "team": "", 
+                "image": '', 
+                "position": "ATK" 
+            },
+            { 
+                "name": "Bolsonaro", 
+                "overall": { 
+                    "pace": "", 
+                    "shooting": "", 
+                    "passing": "", 
+                    "dribble": "", 
+                    "defense": "", 
+                    "physics": "", 
+                    "overall": 53 
+                }, 
+                "country": "", 
+                "team": "", 
+                "image": "", 
+                "position": "DEF" 
+            },
+            { 
+                "name": "Celso", 
+                "overall": { 
+                    "pace": "", 
+                    "shooting": "", 
+                    "passing": "", 
+                    "dribble": "", 
+                    "defense": "", 
+                    "physics": "", 
+                    "overall": 77 
+                }, 
+                "country": "", 
+                "team": NH, 
+                "image": "", 
+                "position": "ATK" 
+            },
+            { 
+                "name": "Danniboy", 
+                "overall": { 
+                    "pace": "", 
+                    "shooting": "", 
+                    "passing": "", 
+                    "dribble": "", 
+                    "defense": "", 
+                    "physics": "", 
+                    "overall": 79 
+                }, 
+                "country": "", 
+                "team": "", 
+                "image": "", 
+                "position": "ATK" 
+            },
+            { 
+                "name": "Daro", 
+                "overall": { 
+                    "pace": "", 
+                    "shooting": "", 
+                    "passing": "", 
+                    "dribble": "", 
+                    "defense": "", 
+                    "physics": "", 
+                    "overall": 46 
+                }, 
+                "country": "", 
+                "team": "", 
+                "image": "", 
+                "position": "MEI" 
+            },
+            { 
+                "name": "Du", 
+                "overall": { 
+                    "pace": "", 
+                    "shooting": "", 
+                    "passing": "", 
+                    "dribble": "", 
+                    "defense": "", 
+                    "physics": "", 
+                    "overall": 53 
+                }, 
+                "country": "", 
+                "team": "", 
+                "image": "", 
+                "position": "DEF" 
+            },
+            { 
+                "name": "Clev", 
+                "overall": { 
+                    "pace": "", 
+                    "shooting": "", 
+                    "passing": "", 
+                    "dribble": "", 
+                    "defense": "", 
+                    "physics": "", 
+                    "overall": 83 
+                }, 
+                "country": "", 
+                "team": "", 
+                "image": "", 
+                "position": "ATK" 
+            },
+            { 
+                "name": "Fer", 
+                "overall": { 
+                    "pace": "", 
+                    "shooting": "", 
+                    "passing": "", 
+                    "dribble": "", 
+                    "defense": "", 
+                    "physics": "", 
+                    "overall": 59 
+                }, 
+                "country": "", 
+                "team": NH, 
+                "image": "", 
+                "position": "MEI" 
+            },
+            { 
+                "name": "Gabriel", 
+                "overall": { 
+                    "pace": "", 
+                    "shooting": "", 
+                    "passing": "", 
+                    "dribble": "", 
+                    "defense": "", 
+                    "physics": "", 
+                    "overall": 59 
+                }, 
+                "country": "", 
+                "team": "", 
+                "image": "", 
+                "position": "MEI" 
+            },
+            { 
+                "name": "Gabrielzin", 
+                "overall": { 
+                    "pace": "", 
+                    "shooting": "", 
+                    "passing": "", 
+                    "dribble": "", 
+                    "defense": "", 
+                    "physics": "", 
+                    "overall": 54 
+                }, 
+                "country": "", 
+                "team": NH, 
+                "image": "", 
+                "position": "MEI" 
+            },
+            { 
+                "name": "Guilherme", 
+                "overall": { 
+                    "pace": "", 
+                    "shooting": "", 
+                    "passing": "", 
+                    "dribble": "", 
+                    "defense": "", 
+                    "physics": "", 
+                    "overall": 33 
+                }, 
+                "country": "", 
+                "team": "", 
+                "image": "", 
+                "position": "DEF" 
+            },
+            { 
+                "name": "Gustavo", 
+                "overall": { 
+                    "pace": "", 
+                    "shooting": "", 
+                    "passing": "", 
+                    "dribble": "", 
+                    "defense": "", 
+                    "physics": "", 
+                    "overall": 55 
+                }, 
+                "country": "", 
+                "team": "", 
+                "image": "", 
+                "position": "ATK" 
+            },
+            { 
+                "name": "Igor", 
+                "overall": { 
+                    "pace": "", 
+                    "shooting": "", 
+                    "passing": "", 
+                    "dribble": "", 
+                    "defense": "", 
+                    "physics": "", 
+                    "overall": 55 
+                }, 
+                "country": "", 
+                "team": NH, 
+                "image": "", 
+                "position": "DEF" 
+            },
+            { 
+                "name": "Jonathan", 
+                "overall": { 
+                    "pace": "", 
+                    "shooting": "", 
+                    "passing": "", 
+                    "dribble": "", 
+                    "defense": "", 
+                    "physics": "", 
+                    "overall": 59 
+                }, 
+                "country": "", 
+                "team": "", 
+                "image": "", 
+                "position": "MEI" 
+            },
+            { 
+              "name": "João da R", 
+              "overall": { 
+                  "pace": "", 
+                  "shooting": "", 
+                  "passing": "", 
+                  "dribble": "", 
+                  "defense": "", 
+                  "physics": "", 
+                  "overall": 37 
+              }, 
+              "country": "", 
+              "team": "", 
+              "image": "", 
+              "position": "ATK" 
+          },
+          { 
+              "name": "Narnia", 
+              "overall": { 
+                  "pace": "", 
+                  "shooting": "", 
+                  "passing": "", 
+                  "dribble": "", 
+                  "defense": "", 
+                  "physics": "", 
+                  "overall": 24 
+              }, 
+              "country": "", 
+              "team": "", 
+              "image": "", 
+              "position": "ATK" 
+          },
+          { 
+              "name": "Denis", 
+              "overall": { 
+                  "pace": "", 
+                  "shooting": "", 
+                  "passing": "", 
+                  "dribble": "", 
+                  "defense": "", 
+                  "physics": "", 
+                  "overall": 95 
+              }, 
+              "country": "", 
+              "team": "", 
+              "image": "", 
+              "position": "MEI" 
+          },
+          { 
+              "name": "Matheus", 
+              "overall": { 
+                  "pace": "", 
+                  "shooting": "", 
+                  "passing": "", 
+                  "dribble": "", 
+                  "defense": "", 
+                  "physics": "", 
+                  "overall": 63 
+              }, 
+              "country": "", 
+              "team": "", 
+              "image": "", 
+              "position": "DEF" 
+          },
+          { 
+              "name": "Preto", 
+              "overall": { 
+                  "pace": "", 
+                  "shooting": "", 
+                  "passing": "", 
+                  "dribble": "", 
+                  "defense": "", 
+                  "physics": "", 
+                  "overall": 90 
+              }, 
+              "country": "", 
+              "team": NH, 
+              "image": "", 
+              "position": "MEI" 
+          },
+          { 
+              "name": "Tonin", 
+              "overall": { 
+                  "pace": "", 
+                  "shooting": "", 
+                  "passing": "", 
+                  "dribble": "", 
+                  "defense": "", 
+                  "physics": "", 
+                  "overall": 44 
+              }, 
+              "country": "", 
+              "team": NH, 
+              "image": "", 
+              "position": "DEF" 
+          }
+          ]
+    )
+    
+} 
