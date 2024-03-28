ab130e714ed70db02bed9a90f38d7325a74a58c2
CelsoPereiraDev <celsopereira.dev@gmail.com>
2024-03-28 17:54:06 -0300

    feat: edit field component to render in realistic formation

--- src/components/Field/index.tsx
+++ src/components/Field/index.tsx
@@ -1,32 +1,52 @@
 import { Player, Team } from "@/utils/createTeam";
 import PlayerCardSmall from "../PlayerCardSmall";
 
-
-export default function Field( { team }:any) {
-  const chunkArray = (array: { slice: (arg0: number, arg1: any) => Player; }, sizes: any[]) => {
-    const chunkedArray: Player[] = [];
-    let index = 0;
-    sizes.forEach(size => {
-      chunkedArray.push(array.slice(index, index + size));
-      index += size;
-    });
+export default function Field({ team }: { team: Team }): JSX.Element {
+  const chunkArray = (players: Player[], size: number) => {
+    const chunkedArray: Player[][] = [];
+    for (let i = 0; i < players.length; i += size) {
+      chunkedArray.push(players.slice(i, i + size));
+    }
     return chunkedArray;
   };
 
-  const playerGroups = chunkArray(team.players, [3, 2, 2]);
+  const groupedPlayers = {
+    ATK: [],
+    MEI: [],
+    DEF: []
+  };
+
+  team.players.forEach((player: Player) => {
+    if (player.position === 'ATK') {
+      groupedPlayers.ATK.push(player);
+    } else if (player.position === 'MEI') {
+      groupedPlayers.MEI.push(player);
+    } else if (player.position === 'DEF') {
+      groupedPlayers.DEF.push(player);
+    }
+  });
+
+  const playerGroups: Player[][] = [];
+  const positions = ['ATK', 'MEI', 'DEF'];
+  positions.forEach(position => {
+    const players = groupedPlayers[position];
+    const chunkedPlayers = chunkArray(players, 3);
+    playerGroups.push(...chunkedPlayers);
+  });
+
+  const gapValue = playerGroups.length === 4 ? 7 : 14;
 
   return (
     <div className="bg-[url('../../public/new_field.jpg')] h-[870px] w-[559px] bg-cover bg-center bg-no-repeat">
-      <div className="flex flex-col h-full justify-end items-center gap-14 pb-[100px]">
+      <div className={`flex flex-col h-full justify-end items-center gap-${gapValue} pb-[100px]`}>
         {playerGroups.map((playersInGroup, groupIndex) => (
           <div key={groupIndex} className="flex flex-row gap-10 justify-center w-[92%]">
             {playersInGroup.map((playerData, index) => (
               <PlayerCardSmall key={index} playerData={playerData} />
-            ))}          
+            ))}
           </div>
         ))}
       </div>
     </div>
   );
 }
-       
