4022c1a0b8ee3a3deeddabc45b2b784cdaa7bd1f
CelsoPereiraDev <celsopereira.dev@gmail.com>
2024-03-29 16:05:21 -0300

    feat: substitute text area for Select

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,11 @@ import { useState } from "react";
 import { calculateTeamOverall, hillClimbing, Player, Team } from "@/utils/createTeam";
 import Field from "@/components/Field";
 import PlayerMock from "@/utils/mockPlayers";
+import Select from 'react-select'
 
 export default function Home() {
   const [teams, setTeams] = useState<Team[]>([]);
+  const [selectedPlayers, setSelectedPlayers] = useState<Player[]>([]);
   const [inputValue, setInputValue] = useState<string>("");
 
   const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -19,44 +21,28 @@ export default function Home() {
   };
 
   const handleGenerateTeams = () => {
-    const players: Player[] = inputValue
-      .split(/[\n,]/)
-      .map((entry) => {
-        const [name, overall] = entry.split(":").map((item) => item.trim());
-        return { name, overall: { overall:parseInt(overall) || 0 } };
-      });
-    
-    const result = hillClimbing(players, 3, 5000);
+    const result = hillClimbing(selectedPlayers, 3, 5000);
     setTeams(result);
   };
 
   const data: Player[] = PlayerMock();
 
-
   return (
     <>
-      <div className="h-100% bg-[#212121] p-12">
+      <div className="h-screen bg-[#212121] p-12">
         <p className="text-3xl text-center mb-9">Gerador de equipes</p>
         <div className="flex flex-col gap-12 text-black  ">
           <div className="flex flex-col items-center space-y-4">
-            <textarea
-              className="p-2 border rounded min-h-[480px] min-w-[480px]"
-              placeholder="Digite os jogadores no formato nome:nota separados por vírgula ou pular linha"
-              rows={5}
-              value={inputValue}
-              onChange={handleInputChange}
+            <Select
+              isMulti
+              options={data}
+              value={selectedPlayers}
+              onChange={(selectedOptions: any) => setSelectedPlayers(selectedOptions)}
             />
             <button
               className="px-4 py-2 bg-blue-500 text-white rounded"
-              onClick={handleGenerateTeams}
-            >
+              onClick={handleGenerateTeams}>
               Gerar Times
-            </button>
-            <button
-              className="px-4 py-2 bg-blue-500 text-white rounded"
-              onClick={handleGenerateTeamsWithMock}
-            >
-              Gerar Times com Mock
             </button>
           </div>
           <div className="flex flex-row items-center space-x-2 w-full justify-around">
