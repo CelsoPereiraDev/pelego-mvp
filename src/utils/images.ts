cf98d52a7f2a048fbb09dbf96fe51db62666a74b
CelsoPereiraDev <celsopereira.dev@gmail.com>
2024-07-03 10:29:34 -0300

    feat: add import image

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -1,9 +1,15 @@
 import flagImage from "@/../public/flag.webp";
 import NHImage from "@/../public/NH.png";
 import Garfinho from "@/../public/garfinho.png"
+import BolsonaroImage from "@/../public/Bolsonaro.png"
+import GuardiolaImage from "@/../public/careca.png"
 
 export const BrazilFlagImage = flagImage;
 
 export const NH = NHImage;
 
 export const GarfinhoImage = Garfinho
+
+export const Bolsonaro = BolsonaroImage;
+
+
