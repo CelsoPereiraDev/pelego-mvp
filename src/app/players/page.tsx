9f0b58289edbf68f1882deb973b3622cf598d4b2
CelsoPereiraDev <celsopereira.dev@gmail.com>
2024-07-03 10:29:13 -0300

    chore: edit allPlayersPage

--- src/app/players/page.tsx
+++ src/app/players/page.tsx
@@ -1,25 +1,22 @@
-import PlayerCard from "@/components/PlayerCard";
 import PlayerCardSmall from "@/components/PlayerCardSmall";
-import { Player } from "@/utils/createTeam";
+import { Player } from "@/types/player";
 import PlayerMock from "@/utils/mockPlayers";
 
-
 export default function AllPlayersPage() {
-    const data: Player[] = PlayerMock();
+  const data: Player[] = PlayerMock();
 
-    return (
-        <div className="h-screen bg-[#212121] w-screen flex justify-start flex-col p-12 items-center gap-7">
-            <h1 className="text-3xl text-center mb-9">Jogadores</h1>
-            <div className="max-w-[1440px] p-6 bg-white h-full rounded-lg">
-                <div className="flex flex-row gap-8 flex-wrap justify-center py-8">
-
-                {data.map((player, index) => (
-                <div className='pb-10' key={index}>
-                    <PlayerCardSmall  key={index} playerData={player} />
-                </div>
-                ))}
-                </div>
+  return (
+    <div className="h-screen bg-[#212121] w-screen flex justify-start flex-col p-12 items-center gap-7">
+      <h1 className="text-3xl text-center mb-9 text-white">Jogadores</h1>
+      <div className="max-w-[1440px] p-6 bg-white h-full rounded-lg overflow-auto">
+        <div className="flex flex-row gap-8 flex-wrap justify-center py-8">
+          {data.map((player, index) => (
+            <div className='pb-10' key={index}>
+              <PlayerCardSmall playerData={player} showOverall={true} />
             </div>
+          ))}
         </div>
-    )
+      </div>
+    </div>
+  );
 }
