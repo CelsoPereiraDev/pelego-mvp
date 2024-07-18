2180eb2f37c3154551b04d9f3f5bdbf7cf20df90
CelsoPereiraDev <celsopereira.dev@gmail.com>
2024-07-18 09:31:42 -0300

    chore: add new types

--- src/types/player.ts
+++ src/types/player.ts
@@ -1,24 +1,31 @@
+import { playerGetOverallSchema } from "@/schema/player";
 import { StaticImageData } from "next/image";
+import { z } from "zod";
 
 export interface PlayerOverall {
-    pace: number;
-    shooting: number;
-    passing: number;
-    dribble: number;
-    defense: number;
-    physics: number;
-    overall: number;
+  pace: number;
+  shooting: number;
+  passing: number;
+  dribble: number;
+  defense: number;
+  physics: number;
+  overall: number;
 }
   
 export interface Player {
-    name: string;
-    overall: PlayerOverall;
-    country: string;
-    label: string;
-    value: string;
-    team: string | StaticImageData;
-    image: string | StaticImageData;
-    position: string;
-    isChampion?: boolean;
-  }
+  name: string;
+  overall: PlayerOverall;
+  country?: string;
+  value?: string;
+  team?: string | StaticImageData;
+  image?: string | StaticImageData;
+  position: string;
+  isChampion?: boolean;
+}
 
+export interface PlayerGoals {
+  name: string;
+  goals: number;
+}
+
+export type PlayerGetOverallFormData = z.infer<typeof playerGetOverallSchema>;
