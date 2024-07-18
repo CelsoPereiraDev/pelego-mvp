809e41ddc9a922ad9aaf944f49b34a31efdbf849
CelsoPereiraDev <celsopereira.dev@gmail.com>
2024-07-18 09:33:24 -0300

    refactor: add react-hook-form and insert schemas

--- src/app/add-player/page.tsx
+++ src/app/add-player/page.tsx
@@ -1,109 +1,157 @@
 'use client'
 
+import CountrySelect, { CountryOption } from "@/components/CountrySelect";
 import PlayerCard from "@/components/PlayerCard";
 import TextInput from "@/components/TextInput";
+import { formToPlayerMapper } from "@/mapper/formToPlayerMapper";
+import { playerGetOverallSchema } from "@/schema/player";
+import { Player, PlayerGetOverallFormData } from "@/types/player";
 import { calculateOverall } from "@/utils/calculateOverall";
-import Select from 'react-select';
+import countryOptions from "@/utils/countryOptions";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
-import { Player } from "@/types/player";
-
-const initialPlayerData: Player = {
-    name: "",
-    overall: {
-        pace: 0,
-        shooting: 0,
-        passing: 0,
-        dribble: 0,
-        defense: 0,
-        physics: 0,
-        overall: 0,
-    },
-    country: "",
-    team: "",
-    image: "",
-    position: "",
-    value: ""
-};
+import { Controller, useForm } from "react-hook-form";
+import Select, { SingleValue } from 'react-select';
 
 export default function AddPlayersPage() {
-    const [playerData, setPlayerData] = useState<Player>(initialPlayerData);
-    
-    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement> | { target: { value: string } }, field: keyof Player, subfield?: keyof Player['overall']) => {
-        const value = event.target.value;
-        if (subfield) {
-            setPlayerData(prevState => ({
-                ...prevState,
-                [field]: {
-                    ...(prevState[field] as Player['overall']),
-                    [subfield]: Number(value)
-                }
-            }));
-        } else {
-            setPlayerData(prevState => ({
-                ...prevState,
-                [field]: value
-            }));
-        }
-    };
-    
-    
-    
-    const positionOptions = [
-        {
-            label: 'ATK',
-            value: 'ATK',
-        },
-        {
-            label: 'MEI',
-            value: 'MEI',
-        },
-        {
-            label: 'DEF',
-            value: 'DEF',
-        }
-    ];
+  const {
+    control,
+    watch,
+    handleSubmit,
+  } = useForm<PlayerGetOverallFormData>({
+    resolver: zodResolver(playerGetOverallSchema),
+  });
 
-    return (
-        <div className="h-screen bg-[#212121] w-screen flex justify-start flex-col p-12 items-center gap-7">
-            <h1 className="text-3xl text-center mb-9">Adicionar Jogador</h1>
-            <div className="max-w-[1440px] p-6 bg-white h-full rounded-lg w-full text-black ">
-                <div>
-                    <div className="flex flex-col gap-1 max-w-[300px]">
-                        <TextInput label='*Nome' value={playerData.name} onChange={(e) => handleInputChange(e, 'name')} />
-                        <TextInput label='*País' value={playerData.country} onChange={(e) => handleInputChange(e, 'country')} />
-                        <div>
-                            <span>*Posição</span>
-                            <Select
-                                options={positionOptions}
-                                value={positionOptions.find(option => option.value === playerData.position) || null}
-                                onChange={(selectedOption) => handleInputChange({ target: { value: selectedOption?.value || '' } }, 'position')}
-                            />
-                        </div>
-                    </div>
-                    <div className="flex flex-row max-w-[300px] mt-4 gap-4 justify-between">
-                        <div className="flex flex-col gap-1">
-                            <TextInput label='*Velocidade' width="40px" value={playerData.overall.pace.toString()} onChange={(e) => handleInputChange(e, 'overall', 'pace')}/>
-                            <TextInput label='*Chute' width="40px" value={playerData.overall.shooting.toString()} onChange={(e) => handleInputChange(e, 'overall', 'shooting')} />
-                            <TextInput label='*Passe' width="40px" value={playerData.overall.passing.toString()} onChange={(e) => handleInputChange(e, 'overall', 'passing')} />
-                        </div>
-                        <div className="flex flex-col gap-1">
-                            <TextInput label='*Drible' width="40px" value={playerData.overall.dribble.toString()} onChange={(e) => handleInputChange(e, 'overall', 'dribble')}/>
-                            <TextInput label='*Defesa' width="40px" value={playerData.overall.defense.toString()} onChange={(e) => handleInputChange(e, 'overall', 'defense')}/>
-                            <TextInput label='*Físico' width="40px" value={playerData.overall.physics.toString()} onChange={(e) => handleInputChange(e, 'overall', 'physics')}/>
-                        </div>
-                    </div>
-                    <button className="px-4 py-2 bg-blue-500 text-white rounded" onClick={() => {
-                        const overallValue = calculateOverall({ position: playerData.position as "ATK" | "MEI" | "DEF", overall: playerData.overall });
-                        setPlayerData(prevState => ({ ...prevState, overall: { ...prevState.overall, overall: overallValue } }));
-                    }}>
-                        Calcular Overall
-                    </button>
-                    <button className="px-4 py-2 bg-blue-500 text-white rounded">
-                        Salvar Jogador
-                    </button>
-                </div>
-                <PlayerCard playerData={playerData} />
+  const overallWatcher = watch('overall');
+  const positionWatcher = watch('position');
+  const playerDataWatcher = watch();
+  const [playerData,setPlayerData] = useState<Player | null>(null)
+
+  const positionOptions = [
+    { label: 'ATK', value: 'ATK' },
+    { label: 'MEI', value: 'MEI' },
+    { label: 'DEF', value: 'DEF' },
+    { label: 'GOL', value: 'GOL' }
+  ];
+
+  const handleCalculateOverall = () => {
+    const overallValue = calculateOverall({ overall: overallWatcher, position: positionWatcher });
+    const formData = formToPlayerMapper(playerDataWatcher,overallValue)
+    setPlayerData(formData)
+};
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="h-screen bg-[#212121] w-screen flex justify-start flex-col p-12 items-center gap-7">
+      <h1 className="text-3xl text-center mb-9">Adicionar Jogador</h1>
+      <div className="max-w-[1440px] p-6 bg-white h-full rounded-lg w-full text-black ">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="flex flex-col gap-1 max-w-[300px]">
+            <Controller
+              control={control}
+              name="name"
+              render={({ field: { onChange, value } }) => (
+                <TextInput label="*Nome" value={value} onChange={onChange} />
+              )}
+            />
+            <div>
+              <span>*Posição</span>
+              <Controller
+                control={control}
+                name="position"
+                render={({ field }) => (
+                  <Select
+                    options={positionOptions}
+                    value={positionOptions.find(option => option.value === field.value) || null}
+                    onChange={(selectedOption) => {
+                      field.onChange(selectedOption?.value || '');
+                    }}
+                  />
+                )}
+              />
             </div>
-        </div>
-    );
+          </div>
+            <div>
+            <label>País</label>
+              <Controller
+                control={control}
+                name="country"
+                render={({ field }) => (
+                  <CountrySelect
+                    value={countryOptions.find(option => option.value === field.value) || null}
+                    onChange={(selectedOption: SingleValue<CountryOption>) => {
+                      field.onChange(selectedOption?.value || '');
+                    }}
+                  />
+                )}
+              />
+            </div>
+          <div className="flex flex-row max-w-[300px] mt-4 gap-4 justify-between">
+            <div className="flex flex-col gap-1">
+              <Controller
+                control={control}
+                name="overall.pace"
+                render={({ field: { onChange, value } }) => (
+                  <TextInput label="*Velocidade" value={value} onChange={onChange} />
+                )}
+              />
+              <Controller
+                control={control}
+                name="overall.shooting"
+                render={({ field: { onChange, value } }) => (
+                  <TextInput label="*Chute" value={value} onChange={onChange} />
+                )}
+              />
+              <Controller
+                control={control}
+                name="overall.passing"
+                render={({ field: { onChange, value } }) => (
+                  <TextInput label="*Passe" value={value} onChange={onChange} />
+                )}
+              />
+            </div>
+            <div className="flex flex-col gap-1">
+              <Controller
+                control={control}
+                name="overall.dribble"
+                render={({ field: { onChange, value } }) => (
+                  <TextInput label="*Drible" value={value} onChange={onChange} />
+                )}
+              />
+              <Controller
+                control={control}
+                name="overall.defense"
+                render={({ field: { onChange, value } }) => (
+                  <TextInput label="*Defesa" value={value} onChange={onChange} />
+                )}
+              />
+              <Controller
+                control={control}
+                name="overall.physics"
+                render={({ field: { onChange, value } }) => (
+                  <TextInput label="*Físico" value={value} onChange={onChange} />
+                )}
+              />
+            </div>
+          </div>
+          <button
+            type="button"
+            className="px-4 py-2 bg-blue-500 text-white rounded"
+            onClick={handleCalculateOverall}
+          >
+            Calcular Overall
+          </button>
+          <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded">
+            Salvar Jogador
+          </button>
+        </form>
+        {
+          playerData && <PlayerCard playerData={playerData} />
+        }
+      </div>
+    </div>
+  );
 }
