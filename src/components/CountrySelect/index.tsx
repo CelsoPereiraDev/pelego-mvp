d49a52525b52698c11bd66d971cbaeb3a3580a92
CelsoPereiraDev <celsopereira.dev@gmail.com>
2024-07-18 09:31:21 -0300

    feat: add SelectCountry

---
+++ src/components/CountrySelect/index.tsx
@@ -0,0 +1,46 @@
+
+import Select, { SingleValue } from 'react-select';
+import Flag from 'react-world-flags';
+import countryOptions from '../../utils/countryOptions';
+
+export interface FormValues {
+  country: string;
+}
+
+export interface CountryOption {
+  value: string;
+  label: string;
+}
+
+interface CountrySelectProps {
+  value: SingleValue<CountryOption>;
+  onChange: (option: SingleValue<CountryOption>) => void;
+}
+
+const customSingleValue = ({ data }: { data: CountryOption }) => (
+  <div style={{ display: 'flex', alignItems: 'center' }}>
+    <Flag code={data.value} style={{ marginRight: 10, height: 15 }} />
+    {data.label}
+  </div>
+);
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const customOption = (props: any) => {
+  const { data, innerRef, innerProps } = props;
+  return (
+    <div ref={innerRef} {...innerProps} style={{ display: 'flex', alignItems: 'center' }}>
+      <Flag code={data.value} style={{ marginRight: 10, height: 20 }} />
+      {data.label}
+    </div>
+  );
+};
+
+const CountrySelect: React.FC<CountrySelectProps> = (props) => (
+  <Select
+    options={countryOptions}
+    components={{ SingleValue: customSingleValue, Option: customOption }}
+    {...props}
+  />
+);
+
+export default CountrySelect;
