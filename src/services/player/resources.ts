5f7a1f07e7068c507c50e84ddccd9e83632b6fa7
CelsoPereiraDev <celsopereira.dev@gmail.com>
2024-07-20 13:20:13 -0300

    refactor: new way of make requisition

--- src/services/player/resources.ts
+++ src/services/player/resources.ts
@@ -1,42 +1,18 @@
-import { CreatePlayerDataRequested, PlayerResponse } from "@/types/player";
-
-import useSWR from 'swr';
-
-const fetcher = (url: string) => fetch(url).then(res => {
-  if (!res.ok) {
-    throw new Error('Erro ao buscar jogadores');
-  }
-  return res.json();
-});
-
-export const usePlayers = () => {
-  const { data, error } = useSWR('http://localhost:3333/api/get_players', fetcher);
-
-  return {
-    players: data,
-    isLoading: !error && !data,
-    isError: error
-  };
-};
+import { CreatePlayerDataRequested, PlayerResponse } from '@/types/player';
+import { QueryRequest } from '@/utils/QueryRequest';
 
 
-export const createPlayer = async (playerData: CreatePlayerDataRequested): Promise<PlayerResponse> => {
-  try {
-    const response = await fetch('http://localhost:3333/api/create_players', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(playerData),
-    });
+const BASE_URL = 'http://localhost:3333/api';
+const CLIENT_ID = 'client-id';
 
-    if (!response.ok) {
-      throw new Error('Erro ao criar jogador');
-    }
+export async function getPlayers() {
+  const queryRequest = new QueryRequest<Array<PlayerResponse>>(BASE_URL, CLIENT_ID);
+  queryRequest.addDefaultHeaders();
+  return queryRequest.get('get_players');
+}
 
-    return await response.json() as PlayerResponse;
-  } catch (error) {
-    console.error('Erro ao criar jogador:', error);
-    throw error;
-  }
-};
+export async function createPlayer(playerData: CreatePlayerDataRequested) {
+  const queryRequest = new QueryRequest<PlayerResponse, CreatePlayerDataRequested>(BASE_URL, CLIENT_ID);
+  queryRequest.addDefaultHeaders();
+  return queryRequest.post('create_players', playerData);
+}
