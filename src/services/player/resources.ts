00d7714449bd9de48986aeac007f5d74842dfb7e
CelsoPereiraDev <celsopereira.dev@gmail.com>
2024-07-20 13:08:33 -0300

    feat: add usePlayers

--- src/services/player/resources.ts
+++ src/services/player/resources.ts
@@ -1,5 +1,25 @@
 import { CreatePlayerDataRequested, PlayerResponse } from "@/types/player";
 
+import useSWR from 'swr';
+
+const fetcher = (url: string) => fetch(url).then(res => {
+  if (!res.ok) {
+    throw new Error('Erro ao buscar jogadores');
+  }
+  return res.json();
+});
+
+export const usePlayers = () => {
+  const { data, error } = useSWR('http://localhost:3333/api/get_players', fetcher);
+
+  return {
+    players: data,
+    isLoading: !error && !data,
+    isError: error
+  };
+};
+
+
 export const createPlayer = async (playerData: CreatePlayerDataRequested): Promise<PlayerResponse> => {
   try {
     const response = await fetch('http://localhost:3333/api/create_players', {
