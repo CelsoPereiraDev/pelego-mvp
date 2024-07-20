5660852cfbeed104fbb9f41ffe6b90b7ddf8a965
CelsoPereiraDev <celsopereira.dev@gmail.com>
2024-07-20 12:49:53 -0300

    feat: add more player types

--- src/types/player.ts
+++ src/types/player.ts
@@ -29,3 +29,34 @@ export interface PlayerGoals {
 }
 
 export type PlayerGetOverallFormData = z.infer<typeof playerGetOverallSchema>;
+
+export interface PlayerResponse {
+  id: string;
+  name: string;
+  overall: OverallDataRequested;
+  country?: string;
+  team?: string;
+  image?: string;
+  position: 'MEI' | 'ATK' | 'DEF' | 'GOL';
+  isChampion: boolean;
+}
+
+export interface OverallDataRequested {
+  pace: number;
+  shooting: number;
+  passing: number;
+  dribble: number;
+  defense: number;
+  physics: number;
+  overall: number;
+}
+
+export interface CreatePlayerDataRequested {
+  name: string;
+  country?: string;
+  team?: string;
+  image?: string;
+  position: 'MEI' | 'ATK' | 'DEF' | 'GOL';
+  overall: OverallDataRequested;
+  isChampion: boolean;
+}
