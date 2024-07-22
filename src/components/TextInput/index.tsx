bc7b2d8abaa93a226fb3ea7c5627e06cbecc06e2
CelsoPereiraDev <celsopereira.dev@gmail.com>
2024-07-22 16:40:05 -0300

    feat: add placeholder on textInput

--- src/components/TextInput/index.tsx
+++ src/components/TextInput/index.tsx
@@ -6,9 +6,10 @@ interface TextInputProps {
     width?: string;
     value?: number | string;
     onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    placeholder?: string;
 }
 
-export default function TextInput({ label, errorMessage, width, value, onChange }: TextInputProps) {
+export default function TextInput({ label, errorMessage, width, value, onChange,placeholder }: TextInputProps) {
 
     const borderColor = errorMessage ? 'border-red-600' : 'border-black';
 
@@ -20,6 +21,7 @@ export default function TextInput({ label, errorMessage, width, value, onChange 
                     className={`border-[1px] ${borderColor} w-[${width}]`}
                     value={String(value)}
                     onChange={onChange}
+                    placeholder={placeholder}
                 />
                 {errorMessage &&
                     <span className="text-xs text-red-600 font-light text-center">{errorMessage}</span>
