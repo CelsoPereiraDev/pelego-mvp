d40d573c6f5c5ba8a99723ebefe754e85423dc3a
CelsoPereiraDev <celsopereira.dev@gmail.com>
2024-07-24 11:51:01 -0300

    chore: edit match type

---
+++ src/types/match.ts
@@ -0,0 +1,49 @@
+import { PlayerResponse } from "@/types/player";
+
+export interface MatchResponse {
+  id: string;
+  date: string;
+  homeTeamId: string;
+  awayTeamId: string;
+  homeTeam: TeamResponse;
+  awayTeam: TeamResponse;
+  result?: MatchResultResponse;
+  goals: GoalResponse[];
+}
+
+export interface CreateMatchDataRequested {
+  date: string;
+  homeTeamId: string;
+  awayTeamId: string;
+  homeGoals: GoalDetails[];
+  awayGoals: GoalDetails[];
+}
+
+export interface GoalDetails {
+  playerId: string;
+  goals: number;
+}
+
+export interface TeamResponse {
+  id: string;
+  name: string;
+  players: PlayerResponse[];
+}
+
+export interface CreateWeekWithTeamsBody {
+  date: string;
+  teams: string[][];
+}
+
+export interface MatchResultResponse {
+  homeGoals: number;
+  awayGoals: number;
+}
+
+export interface GoalResponse {
+  id: string;
+  matchId: string;
+  playerId: string;
+  match: MatchResponse;
+  player: PlayerResponse;
+}
