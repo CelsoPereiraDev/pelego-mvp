4d3de833315d3891c7e29dcd1e7034d43df98b2c
CelsoPereiraDev <celsopereira.dev@gmail.com>
2024-07-24 11:51:18 -0300

    feat: add edit type on UsePlayer

--- src/services/player/usePlayer.ts
+++ src/services/player/usePlayer.ts
@@ -1,6 +1,6 @@
-import { PlayerResponse } from '@/types/player';
+import { CreatePlayerDataRequested, PlayerResponse } from '@/types/player';
 import useSWR, { useSWRConfig } from 'swr';
-import { deletePlayer, getPlayer } from './resources';
+import { deletePlayer, editPlayer, getPlayer } from './resources';
 
 export function usePlayer(playerId: string, initialData?: PlayerResponse) {
   const { data, error, isLoading } = useSWR(playerId ? `/api/get_player/${playerId}` : null, () => getPlayer(playerId), {
@@ -14,11 +14,17 @@ export function usePlayer(playerId: string, initialData?: PlayerResponse) {
       await deletePlayer(playerId);
       mutate(`/api/get_players`);
     },
+    edit: async function _edit(playerData: CreatePlayerDataRequested) {
+      const updatedPlayer = await editPlayer(playerId, playerData);
+      mutate(`/api/get_player/${playerId}`, updatedPlayer, false);
+      return updatedPlayer;
+    },
   };
 
   return {
     player: data,
     delete: methodsHandler.delete,
+    edit: methodsHandler.edit,
     isLoading,
     isError: error,
   };
