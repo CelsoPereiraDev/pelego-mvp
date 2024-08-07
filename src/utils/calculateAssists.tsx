81cfbbb87cd257c81fc6b2537b3167548b742a44
CelsoPereiraDev <celsopereira.dev@gmail.com>
2024-08-07 11:07:29 -0300

    feat: add calculate assists function

---
+++ src/utils/calculateAssists.tsx
@@ -0,0 +1,44 @@
+import { PlayerResponse } from "@/types/player";
+import { WeekResponse } from "@/types/weeks";
+
+export interface SimpleAssistStats {
+  name: string;
+  assists: number;
+}
+
+export const calculateSimpleAssistStats = (weeks: WeekResponse[]): SimpleAssistStats[] => {
+  const assistStatsMap: { [playerId: string]: SimpleAssistStats } = {};
+
+  weeks?.forEach((week) => {
+    week.teams?.flatMap((team) => team.matchesHome?.concat(team.matchesAway) ?? []).forEach((match) => {
+      const processedMatches = new Set<string>();
+      
+      if (!processedMatches.has(match.id)) {
+        processedMatches.add(match.id);
+
+        const allPlayers = new Set<PlayerResponse>();
+        match.assists?.forEach(assist => {
+          if (assist.player) allPlayers.add(assist.player);
+        });
+
+        allPlayers.forEach(player => {
+          if (!assistStatsMap[player.id]) {
+            assistStatsMap[player.id] = {
+              name: player.name,
+              assists: 0,
+            };
+          }
+        });
+
+        match.assists?.forEach(assist => {
+          if (assist.playerId) {
+            assistStatsMap[assist.playerId].assists += assist.assists;
+          }
+        });
+      }
+    });
+  });
+
+  // Convert to array and return
+  return Object.values(assistStatsMap).sort((a, b) => b.assists - a.assists);
+};
