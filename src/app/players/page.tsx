c4aea42ad17b5315c46a478b7e0c4b1e4bdfc158
CelsoPereiraDev <celsopereira.dev@gmail.com>
2024-08-07 11:06:56 -0300

    feat: add router and function to go to player page

--- src/app/players/page.tsx
+++ src/app/players/page.tsx
@@ -2,9 +2,15 @@
 
 import PlayerCardSmall from "@/components/PlayerCardSmall";
 import { usePlayers } from "@/services/player/usePlayers";
+import { useRouter } from "next/navigation";
 
 export default function AllPlayersPage() {
-  const { players } = usePlayers()
+  const { players } = usePlayers();
+  const router = useRouter();
+
+   const goToPlayerPage = (weekId: string) => {
+    router.push(`/player/${weekId}`);
+  };
 
   return (
     <div className="h-screen bg-[#212121] w-screen flex justify-start flex-col p-12 items-center gap-7">
@@ -12,7 +18,7 @@ export default function AllPlayersPage() {
       <div className=" p-6 bg-white h-full rounded-lg overflow-auto">
         <div className="flex flex-row gap-8 flex-wrap justify-start py-8">
           {players?.map((player, index) => (
-            <div className='pb-10' key={index}>
+            <div className='pb-10' key={index} onClick={() => goToPlayerPage(player.id)}>
               <PlayerCardSmall playerData={player} showOverall={true} />
             </div>
           ))}
