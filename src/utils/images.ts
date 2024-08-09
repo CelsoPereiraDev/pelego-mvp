4fe96f1dee3dfc0ea8944718bf857ade5f3cb1e4
CelsoPereiraDev <celsopereira.dev@gmail.com>
2024-08-09 16:54:56 -0300

    feat: add images

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -1,6 +1,7 @@
 import BolsonaroImage from "@/../public/Bolsonaro.png";
 import StrikerMVPImage from "@/../public/chuteira_mvp.svg";
 import StrikerNormalImage from "@/../public/chuteira_simple.svg";
+import StrikerMonthImage from "@/../public/chuteira_striker.svg";
 import StrikerWeekImage from "@/../public/chuteira_week.svg";
 import flagImage from "@/../public/flag.webp";
 import Garfinho from "@/../public/garfinho.png";
@@ -21,6 +22,10 @@ export const StrikerWeek = StrikerWeekImage;
 
 export const StrikerMVP = StrikerMVPImage;
 
-export const monthWinner = monthWinnerImage
+export const monthWinner = monthWinnerImage;
+
+export const StrikerMonth = StrikerMonthImage
+
+
 
 
